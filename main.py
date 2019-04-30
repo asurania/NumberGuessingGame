d6e87485d78e9c9e5ece3b813b82e8e228744992
Azim Surani <asurania@gmail.com>
2019-04-30 00:50:54 -0600

    renaming function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import random
+
 guess_list = []
 guess = None
 highscore = 0
 
-def number_game():
-    import random
+
+def start_game():
     global guess
     global guess_list
     global play
@@ -16,7 +18,8 @@ def number_game():
             guess = int(input("{} What is your Guess, Pick a Number 1 to 10:  ".format(name)))
 
             if guess > 10 or guess < 1:
-                raise ValueError("There has been an ERROR. {} the Number you have choosen is NOT between 1 and 10".format(name))
+                raise ValueError(
+                    "There has been an ERROR. {} the Number you have choosen is NOT between 1 and 10".format(name))
 
             if guess == random_number:
                 print("You win")
@@ -29,7 +32,7 @@ def number_game():
                     guess_list = []
                     guess = None
                 print("The highscore is {}".format(highscore))
-                play = input("{} Do you want to Play the Number Game Again. Y to play again:  ".format(name))
+                play = input("{} Press (Y)es to play again or any other key to quit:  ".format(name))
                 random_number = (random.randint(1, 10))
 
             elif guess > random_number:
@@ -42,9 +45,11 @@ def number_game():
                 print("This is your {} attempt".format(len(guess_list)))
         except ValueError as err:
             print("{} There has been an Error. You have not entered a number betwen 1 and 10".format(name))
-    print ("Have a nice day")
+    print("Goodbye, Have a nice day")
 
-print("Welcome to The Number Guessing Game")
-name = input("What is your Name: ")
-play = input("{} Do you want to Play the Number Game. Press Y to play:  ".format(name))
-number_game()
+
+if __name__ == '__main__':
+    print("Welcome to The Number Guessing Game")
+    name = input("What is your Name: ")
+    play = input("{} Do you want to Play the Number Game. Press Y to play:  ".format(name))
+    start_game()
